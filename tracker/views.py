f69565d4de5552f98a12e262a1991741d4ee6dd2
Cade Colvin <cade.colvin@gmail.com>
2017-09-20 14:21:34 -0500

    Created the tracker index view

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Item, Comment
+
+def index(request):
+    items = Item.objects.order_by('-created_date')
+    context = {'items': items}
+    return render(request, 'tracker/index.html', context)
+
