526ee1866155ddcd598f3dd3982005286a5f1b54
Cade Colvin <cade.colvin@gmail.com>
2017-09-20 11:23:50 -0500

    Added base models

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=128)
+    created_date = models.DateField(auto_now=False, auto_now_add=True)
+    completed_date = models.DateField(auto_now=False, auto_now_add=False)
+
+    def __str__(self):
+        return self.title
+
+    def __repr__(self):
+        return '{i}:{t}'.format(i=self.id, t=self.title)
+
+    class Meta:
+        db_table = 'Items'
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    created_date = models.DateField(auto_now=False, auto_now_add=True)
+    item = models.ForeignKey(Item, related_name='comments', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
+
+    def __repr__(self):
+        return self.id
+
+    class Meta:
+        db_table = 'Comments'
--- tracker/tests.py
+++ tracker/tests.py
@@ -1,3 +1,18 @@
+from .models import Item, Comment
 from django.test import TestCase
 
-# Create your tests here.
+
+class ItemTests(TestCase):
+
+    def test_str(self):
+        test_title = 'Test Title 1'
+        item = Item(title=test_title)
+        self.assertEqual(item.__str__(), test_title)
+
+
+class CommentTests(TestCase):
+
+    def test_str(self):
+        test_text = 'This is simply a test'
+        comment = Comment(text=test_text)
+        self.assertEqual(comment.__str__(), test_text)
